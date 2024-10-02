a25b5488e41f93c873e8ef9010c828f325e52462
Osama Ahmad <osama.nxb@gmail.com>
2024-10-03 00:04:04 +0500

    Initial Commit

---
+++ Question 01 - Sorting.cs
@@ -0,0 +1,29 @@
+﻿/// Question 1 Without using any kind of .Sort or OrderBy Method or touching the constructor re-arrange the Numbers list into numerical order
+/// Hint: I'm looking for loops
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    public class Question1
+    {
+        public List<int> Numbers { get; set; }
+
+        public Question14()
+        {
+            Numbers = new List<int>()
+            {
+                1,9,32,5,2,6,4,2,7
+            };    
+        }
+
+        public void Answer()
+        {
+            
+        }
+    }
+}
---
+++ Question 03 - Code Review.cs
@@ -0,0 +1,55 @@
+﻿// Question 3 - Code review the code below. Write any comments / suggestions in code comments next to the affected line.
+// if you feel like your comment can be backed up with evidence: eg 'this breaks the open closed principle' - please point to any evidence via MSDN, 
+// blogposts etc
+
+
+//Start code review HERE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace InterviewQuestions
+{
+    public class Question3
+    {
+        public string defaultAnswer;
+        private List<string> AvaliableAnswers { get; set; }
+
+
+        public Question7(List<string> avaliableAnswers)
+        {
+            defaultAnswer = GetDefaultAnswer(avaliableAnswers);
+        }
+
+        public string GetDefaultAnswer(List<string> avaliableAnswers)
+        {
+            try
+            {
+                // we shouldn't have more than one avaliable answer NORMALLY, so lets try and get one.
+                return avaliableAnswers.Single();
+            }
+            catch (Exception e)
+            {
+                if (avaliableAnswers.Count == 0)
+                {
+                    return "There are no answers avaliable";
+                }
+                else
+                {
+                    // there were multiple answers. Inform the user
+                    return "There are more than 1 answers to this question";
+                }
+            }
+        }
+
+        public void DisplayAnswersToUser()
+        {
+            foreach (var answer in AvaliableAnswers)
+            {
+                var currentItemPosition = AvaliableAnswers.IndexOf(answer) + 1;
+                Console.WriteLine(currentItemPosition + " - " + answer);
+            }
+        }
+    }
+}
